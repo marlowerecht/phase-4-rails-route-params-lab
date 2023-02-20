539eea75c818750c9ac95a04386be1c0100d6215
Marlowe Recht <recht.marlowe@gmail.com>
2023-02-20 20:55:39 +0100

    commit tree e04905f8cf68fbb6a1858662ac2d7596f5127ccc

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,14 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  def find_by_name name
+    students = Student.filter { |student| student.first_name.include?(name)}
+    render json: students
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
+  get 'students/find-by-name/:name', to: 'students#find_by_name'
+  get '/students/:id', to: 'students#show'
 end
